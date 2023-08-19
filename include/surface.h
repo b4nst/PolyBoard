4d1f16959e019fcb4fb2abb3b3fb3bc4471daae2
banst <bastien@loftorbital.com>
2023-08-19 21:55:24 +0200

    style: fmt

--- include/surface.h
+++ include/surface.h
@@ -1,13 +1,10 @@
 #ifndef SURFACE_H
 #define SURFACE_H
 
-enum SurfaceKeyType {
-	SURFACE_PAD,
-	SURFACE_CTRL
-};
+enum SurfaceKeyType { SURFACE_PAD, SURFACE_CTRL };
 
 /**
- * @brief index_to_surface_key_type Convert an index to a SurfaceKeyType 
+ * @brief index_to_surface_key_type Convert an index to a SurfaceKeyType
  * @param index The index to convert
  * @return The SurfaceKeyType
  */
--- src/surface.c
+++ src/surface.c
@@ -1,10 +1,18 @@
 #include "surface.h"
 
 enum SurfaceKeyType index_to_surface_key_type(unsigned char index) {
-    if (index >= 90) { return SURFACE_CTRL; }
-    if (index <= 10) { return SURFACE_CTRL; }
-    if (index % 10 == 9) { return SURFACE_CTRL; }
-    if (index % 10 == 1) { return SURFACE_CTRL; }
-    
-    return SURFACE_PAD; 
+  if (index >= 90) {
+    return SURFACE_CTRL;
+  }
+  if (index <= 10) {
+    return SURFACE_CTRL;
+  }
+  if (index % 10 == 9) {
+    return SURFACE_CTRL;
+  }
+  if (index % 10 == 1) {
+    return SURFACE_CTRL;
+  }
+
+  return SURFACE_PAD;
 }
